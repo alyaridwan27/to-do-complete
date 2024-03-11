833d71b00979b732f8b58c93c6e2a2e377e2143e
unknown <alyanursalmaridwan@gmail.com>
2024-03-11 21:41:01 +0700

    initial commit

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBOEyLoSKA3-QaQeAkI9gLmTOcR1mWN9TA",
+    authDomain: "todoapp-5adba.firebaseapp.com",
+    projectId: "todoapp-5adba",
+    storageBucket: "todoapp-5adba.appspot.com",
+    messagingSenderId: "1090617847032",
+    appId: "1:1090617847032:web:17528c8043ecebd41c5f0d",
+    measurementId: "G-7H555FRCFR"
+};
